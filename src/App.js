606f17b3c10aa13c09c80c66b83e4d463d12175a
itzsarahhx3 <sarahzhan522@gmail.com>
2021-07-06 15:57:22 -0400

    {fix}: pwa conflict

--- src/App.js
+++ src/App.js
@@ -2,26 +2,6 @@ import React from "react";
 import Routes from "./Routes";
 
 const App = () => {
-    if(!window.Promise) {
-        window.Promise = Promise;
-    }
-
-    if ('serviceWorker' in navigator) {
-        navigator.serviceWorker
-            .register('../../sw.js')
-            .then(function() {
-                console.log('Service worker registered!')
-            })
-            .catch(function(err){
-                console.log('[Error] ', err)
-            });
-    }
-
-    window.addEventListener('beforeinstallprompt', function(evt) {
-        evt.preventDefault();
-        // let deferredPrompt = evt;
-        return false;
-    });
 
     return (
         <div>

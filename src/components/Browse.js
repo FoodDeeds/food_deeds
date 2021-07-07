0c5953fe91a9918f965cbb00f33d73d9ba5cc6e3
Pauline Tannos <ptannos2@gmail.com>
2021-07-07 16:24:05 -0400

    {feat}: browse page renders data from firebase

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,16 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import MapSearch from "./MapSearch";
+import { db } from "../firebase";
 
 const Browse = (props) => {
   const [category, setCategory] = useState("");
   const [zipcode, setZipcode] = useState("");
+  const [donations, setDonations] = useState([]);
 
   const submit = (evt) => {
     evt.preventDefault();
+    db.collection("Donations")
+      //   .where("PostalCode", "==", zipcode)
+      //   .orderBy("timestamp", "desc")
+      .onSnapshot((snapshot) => {
+        setDonations(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            info: doc.data(),
+          }))
+        );
+        console.log(snapshot.docs);
+      });
   };
 
-  console.log("category", category);
-  console.log("zipcode", zipcode);
   return (
     <div className="browse">
       <form onSubmit={submit}>
@@ -31,7 +43,26 @@ const Browse = (props) => {
         </select>
         <button type="submit">Search</button>
       </form>
+      <br />
       <MapSearch />
+      <div className="search-results">
+        <h3>Showing results:</h3>
+        {donations.map((donation) => (
+          <div className="result">
+            <p>
+              {donation.info.Address} <br />
+              {donation.info.City}, {donation.info.State}{" "}
+              {donation.info.PostalCode}
+              <br />
+              Pickup Time: {donation.info.PickupTime} <br />
+              Pickup Date: {donation.info.PickupDate} <br />
+              Quantity: {donation.info.Quantity} boxes
+              <br />
+              <button>Reserve</button>
+            </p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };

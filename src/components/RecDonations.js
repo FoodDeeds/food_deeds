b6d37b9fbb4a70f211d5333d012ee67cc335e3c7
Pauline Tannos <ptannos2@gmail.com>
2021-07-10 10:51:57 -0400

    {feat}: donations on home page

--- src/components/RecDonations.js
+++ src/components/RecDonations.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { db } from "../firebase";
 
 /**
  * user's icon, name
@@ -7,14 +8,45 @@ import React from 'react'
  * Pass them as props by PostDonation (parent)
  */
 
-const RecDonations = ()=> {
+const RecDonations = () => {
+  const [donations, setDonations] = useState([]);
 
-        return (
-            <div>
-                Currently Available Donations by Supplier
-            </div>
-        )
+  useEffect(() => {
+    db.collection("Donations")
+      // .where("PostalCode", "==", zipcode)
+      // .orderBy("Timestamp", "desc")
+      .onSnapshot((snapshot) => {
+        setDonations(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            info: doc.data(),
+          }))
+        );
+      });
+  }, []);
 
-}
+  console.log("Donations!!!", donations);
+  return (
+    <div>
+      <h3>Currently Available Donations by Supplier</h3>
+      {donations.map((donation) => (
+        <div className="result" key={donation.id}>
+          <p>
+            {donation}
+            {donation.info.Address} <br />
+            {donation.info.City}, {donation.info.State}{" "}
+            {donation.info.PostalCode}
+            <br />
+            Pickup Time: {donation.info.PickupTime} <br />
+            Pickup Date: {donation.info.PickupDate} <br />
+            Quantity: {donation.info.Quantity} boxes
+            <br />
+            <button>Reserve</button>
+          </p>
+        </div>
+      ))}
+    </div>
+  );
+};
 
-export default RecDonations
+export default RecDonations;

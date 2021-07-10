5850dd84e6bd21c27433766793316001c96cd6b7
Hadeer Yousef <80984014+Hadeer-Yousef@users.noreply.github.com>
2021-07-10 17:36:32 -0400

    {style}: SideBar

--- src/Routes.js
+++ src/Routes.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import Header from "./components/Header";
 import Home from "./components/Home";
 import MapSearch from "./components/MapSearch";
 import LogIn from "./components/LogIn";
@@ -22,10 +21,6 @@ import Confirmation from "./components/Confirmation";
 const Routes = () => {
   return (
     <Router>
-      <div className="header">
-        <Header />
-      </div>
-
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/map" component={MapSearch} />

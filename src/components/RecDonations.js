aee040baaa852d02aa19c8c8a50463e3653a4dfd
Pauline Tannos <ptannos2@gmail.com>
2021-07-10 11:55:59 -0400

    {fix}: need to log in to post and fixed addresses

--- src/components/RecDonations.js
+++ src/components/RecDonations.js
@@ -9,57 +9,58 @@ import { db } from "../firebase";
  */
 
 const RecDonations = () => {
-    const [donations, setDonations] = useState([]);
-    const [supplierInfo, setSupplierInfo] = useState({});
+  const [donations, setDonations] = useState([]);
+  const [supplierInfo, setSupplierInfo] = useState({});
 
-    useEffect(() => {
-        db.collection("Donations")
-            // .where("PostalCode", "==", zipcode)
-            // .orderBy("Timestamp", "desc")
-            .onSnapshot((snapshot) => {
-                setDonations(
-                    snapshot.docs.map((doc) => ({
-                        id: doc.id,
-                        info: doc.data()
-                    }))
-                );
-            });
+  useEffect(() => {
+    db.collection("Donations")
+      // .where("PostalCode", "==", zipcode)
+      // .orderBy("Timestamp", "desc")
+      .onSnapshot((snapshot) => {
+        setDonations(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            info: doc.data(),
+          }))
+        );
+      });
 
-        donations.forEach((donation) => {
-            db.collection("SignedUpUsers")
-                .doc(donation.info.supplierId)
-                .get()
-                .then((response) => {
-                    const data = response.data();
-                    setSupplierInfo(data);
-                });
+    donations.forEach((donation) => {
+      db.collection("SignedUpUsers")
+        .doc(donation.info.supplierId)
+        .get()
+        .then((response) => {
+          const data = response.data();
+          setSupplierInfo(data);
         });
-    }, []);
-    // console.log("supplier info data>>>>>", supplierInfo);
+    });
+  }, []);
+  // console.log("supplier info data>>>>>", supplierInfo);
 
-    console.log("Donations!!!", donations);
-    return (
-        <div>
-            <h3>Currently Available Donations by Supplier</h3>
-            {donations.map((donation) => (
-                <div className="result" key={donation.id}>
-                    <p>
-                        <img src={donation.info.postImageUrl} alt="" /> <br />
-                        {donation.info.supplierName} <br />
-                        {donation.info.Description} <br />
-                        {donation.info.City}, {donation.info.State}
-                        {donation.info.PostalCode}
-                        <br />
-                        Pickup Time: {donation.info.PickupTime} <br />
-                        Pickup Date: {donation.info.PickupDate} <br />
-                        Quantity: {donation.info.Quantity} boxes
-                        <br />
-                        <button>Reserve</button>
-                    </p>
-                </div>
-            ))}
+  console.log("Donations!!!", donations);
+  return (
+    <div>
+      <h3>Currently Available Donations by Supplier</h3>
+      {donations.map((donation) => (
+        <div className="result" key={donation.id}>
+          <p>
+            <img src={donation.info.postImageUrl} alt="" /> <br />
+            {donation.info.supplierName} <br />
+            {donation.info.Description} <br />
+            {/* removed city state zipcode from form, data needs to come from somewhere else */}
+            {/* {donation.info.City}, {donation.info.State}
+                        {donation.info.PostalCode} */}
+            <br />
+            Pickup Time: {donation.info.PickupTime} <br />
+            Pickup Date: {donation.info.PickupDate} <br />
+            Quantity: {donation.info.Quantity} boxes
+            <br />
+            <button>Reserve</button>
+          </p>
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
 export default RecDonations;

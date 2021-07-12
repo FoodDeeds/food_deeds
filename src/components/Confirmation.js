3cf2246c52a68f90c1a61830c671b88f955cf516
Pauline Tannos <ptannos2@gmail.com>
2021-07-12 17:30:21 -0400

    {feat}: Reserve and release buttons

--- src/components/Confirmation.js
+++ src/components/Confirmation.js
@@ -1,20 +1,58 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Button } from "semantic-ui-react";
 import { useHistory } from "react-router";
+import { db, auth } from "../firebase";
 
 const Confirmation = ({ location }) => {
   const supplierInfo = location.state.supplierInfo;
   const donation = location.state.donation;
+  const [release, setRelease] = useState(true);
+
+  const [currentUser, setCurrentUser] = useState({});
+
   console.log("donation", donation);
   const history = useHistory();
 
-  const handleClick = () => {
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        setCurrentUser(user);
+      }
+    });
+  }, []);
+
+  const handleRelease = () => {
     console.log("clicked release");
+    setRelease(false);
+    db.collection("Donations").doc(donation.id).set(
+      {
+        Status: true,
+        recipientId: null,
+      },
+      { merge: true }
+    );
+  };
+
+  const handleReserve = () => {
+    setRelease(true);
+    db.collection("Donations").doc(donation.id).set(
+      {
+        Status: false,
+        recipientId: currentUser.uid,
+      },
+      {
+        merge: true,
+      }
+    );
   };
 
   return (
     <div>
-      <h2> Pick-up Confirmed </h2>
+      {release === true ? (
+        <h2> Pick-up Confirmed </h2>
+      ) : (
+        <h2>Order Cancelled</h2>
+      )}
       <p> Pick-up Date: {donation.info.PickupDate}</p>
       <p> Quantity:{donation.info.Quantity} boxes</p>
       <br />
@@ -28,10 +66,15 @@ const Confirmation = ({ location }) => {
       </p>
       <p> Phone Number: {supplierInfo.Phone} </p>
       <br />
-
-      <Button color="green" onClick={handleClick}>
-        Release
-      </Button>
+      {release === true ? (
+        <Button color="red" onClick={handleRelease}>
+          Release
+        </Button>
+      ) : (
+        <Button color="green" onClick={handleReserve}>
+          Reserve
+        </Button>
+      )}
     </div>
   );
 };

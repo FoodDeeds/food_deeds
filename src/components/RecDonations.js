1f5da61b2e8c7c9547e6735532992f986b57904c
Hadeer Yousef <80984014+Hadeer-Yousef@users.noreply.github.com>
2021-07-14 17:39:58 -0400

    {feat}: Added toast notification in RecDonations component when a guest clicks on reserve

--- src/components/RecDonations.js
+++ src/components/RecDonations.js
@@ -2,7 +2,8 @@ import React, { useState, useEffect } from "react";
 import { auth, db } from "../firebase";
 import { Button, Item } from "semantic-ui-react";
 import { useHistory, Link } from "react-router-dom";
-
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import post from "../images/post-default.png";
 
 /**
@@ -13,124 +14,130 @@ import post from "../images/post-default.png";
  */
 
 const RecDonations = () => {
-    const [currentUser, setCurrentUser] = useState("");
-    const [donations, setDonations] = useState([]);
-    const [supplierInfo, setSupplierInfo] = useState({});
-    const history = useHistory();
+  const [currentUser, setCurrentUser] = useState("");
+  const [donations, setDonations] = useState([]);
+  const [supplierInfo, setSupplierInfo] = useState({});
+  const history = useHistory();
 
-    useEffect(() => {
-        auth.onAuthStateChanged((user) => {
-            if (user) {
-                setCurrentUser(user);
-            }
-        });
-        db.collection("Donations")
-            .where("Status", "==", true)
-            // .orderBy("Timestamp", "desc")
-            .onSnapshot((snapshot) => {
-                setDonations(
-                    snapshot.docs.map((doc) => ({
-                        id: doc.id,
-                        info: doc.data()
-                    }))
-                );
-            });
-        db.collection("Donations")
-            .where("Status", "==", true)
-            .onSnapshot((snapshot) => {
-                setDonations(
-                    snapshot.docs.map((doc) => ({
-                        id: doc.id,
-                        info: doc.data()
-                    }))
-                );
-            });
-    }, []);
-
-    const handleClick = (donation) => {
-        setDonations(donation);
-        console.log("donation in home", donation.id);
-        db.collection("Donations").doc(donation.id).set(
-            {
-                Status: false,
-                recipientId: currentUser.uid
-            },
-            { merge: true }
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        setCurrentUser(user);
+      }
+    });
+    db.collection("Donations")
+      .where("Status", "==", true)
+      // .orderBy("Timestamp", "desc")
+      .onSnapshot((snapshot) => {
+        setDonations(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            info: doc.data(),
+          }))
         );
-        history.push({
-            pathname: "/confirmation",
-            state: {
-                donation,
-                supplierInfo
-            }
-        });
-    };
+      });
+    db.collection("Donations")
+      .where("Status", "==", true)
+      .onSnapshot((snapshot) => {
+        setDonations(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            info: doc.data(),
+          }))
+        );
+      });
+  }, []);
+  toast.configure();
+  const showToast = () => {
+    toast("Please log in to reserve!", {
+      position: "top-center",
+      autoClose: 4000,
+    });
+  };
+  const handleClick = (donation) => {
+    if (currentUser) {
+      setDonations(donation);
+      console.log("donation in home", donation.id);
+      db.collection("Donations").doc(donation.id).set(
+        {
+          Status: false,
+          recipientId: currentUser.uid,
+        },
+        { merge: true }
+      );
+      history.push({
+        pathname: "/confirmation",
+        state: {
+          donation,
+          supplierInfo,
+        },
+      });
+    } else {
+      showToast();
+    }
+  };
 
-    console.log("Donations!!!", donations);
+  console.log("Donations!!!", donations);
 
-    return (
-        <div>
-            <h3>Currently Available For Pick-Up</h3>
-            {donations.map((donation) => (
-                <div className="result" key={donation.id}>
-                    <Item.Group divided style={{ marginLeft: 30 }}>
-                        <Item>
-                            <br />
-                            {donation.info.postImageUrl ? (
-                                <Item.Image
-                                    src={donation.info.postImageUrl}
-                                    alt=""
-                                    style={{ marginRight: 20, marginTop: 10 }}
-                                />
-                            ) : (
-                                <Item.Image
-                                    src={post}
-                                    alt=""
-                                    style={{ marginRight: 20, marginTop: 10 }}
-                                />
-                            )}
-                            <br />
-                            <Item.Content>
-                                <Link
-                                    to={`/supplier/${donation.info.supplierId}`}
-                                >
-                                    <Item.Header as="a">
-                                        {donation.info.supplierName}
-                                    </Item.Header>
-                                </Link>
-                                <Item.Meta>
-                                    Last Available Pick Up Time: <br />
-                                    {donation.info.PickupTime} on{" "}
-                                    {donation.info.PickupDate} <br />
-                                </Item.Meta>
-                                Quantity: {donation.info.Quantity} boxes
-                                <Item.Description>
-                                    {" "}
-                                    Description: {donation.info.Description}
-                                </Item.Description>
-                                {/* removed city state zipcode from form, data needs to come from somewhere else */}
-                                {/* {donation.info.City}, {donation.info.State}
+  return (
+    <div>
+      <h3>Currently Available For Pick-Up</h3>
+      {donations.map((donation) => (
+        <div className="result" key={donation.id}>
+          <Item.Group divided style={{ marginLeft: 30 }}>
+            <Item>
+              <br />
+              {donation.info.postImageUrl ? (
+                <Item.Image
+                  src={donation.info.postImageUrl}
+                  alt=""
+                  style={{ marginRight: 20, marginTop: 10 }}
+                />
+              ) : (
+                <Item.Image
+                  src={post}
+                  alt=""
+                  style={{ marginRight: 20, marginTop: 10 }}
+                />
+              )}
+              <br />
+              <Item.Content>
+                <Link to={`/supplier/${donation.info.supplierId}`}>
+                  <Item.Header as="a">{donation.info.supplierName}</Item.Header>
+                </Link>
+                <Item.Meta>
+                  Last Available Pick Up Time: <br />
+                  {donation.info.PickupTime} on {donation.info.PickupDate}{" "}
+                  <br />
+                </Item.Meta>
+                Quantity: {donation.info.Quantity} boxes
+                <Item.Description>
+                  {" "}
+                  Description: {donation.info.Description}
+                </Item.Description>
+                {/* removed city state zipcode from form, data needs to come from somewhere else */}
+                {/* {donation.info.City}, {donation.info.State}
                         {donation.info.PostalCode} */}
-                                <br />
-                            </Item.Content>
-                            <Button
-                                basic
-                                color="green"
-                                style={{
-                                    width: 100,
-                                    height: 30,
-                                    marginRight: 20
-                                }}
-                                onClick={() => handleClick(donation)}
-                            >
-                                Reserve
-                            </Button>
-                        </Item>
-                    </Item.Group>
-                </div>
-            ))}
+                <br />
+              </Item.Content>
+              <Button
+                basic
+                color="green"
+                style={{
+                  width: 100,
+                  height: 30,
+                  marginRight: 20,
+                }}
+                onClick={() => handleClick(donation)}
+              >
+                Reserve
+              </Button>
+            </Item>
+          </Item.Group>
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
 export default RecDonations;

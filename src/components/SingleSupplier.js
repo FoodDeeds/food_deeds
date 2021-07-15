b6fe89e8a947329a34c4221ca41f0573423b6e68
Hadeer Yousef <80984014+Hadeer-Yousef@users.noreply.github.com>
2021-07-15 15:49:18 -0400

    {feat}: Added toast notification for guests when they click on reserve

--- src/components/SingleSupplier.js
+++ src/components/SingleSupplier.js
@@ -4,6 +4,8 @@ import { useHistory } from "react-router-dom";
 import { Header, Item, Segment } from "semantic-ui-react";
 import { Button } from "semantic-ui-react";
 import logo from "../images/Logo-2.png";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const SingleSupplier = (props) => {
   const [supplierInfo, setSupplierInfo] = useState({});
@@ -41,24 +43,35 @@ const SingleSupplier = (props) => {
         );
       });
   }, [props.match.params.id]);
+  toast.configure();
+  const showToast = () => {
+    toast("Please log in to reserve!", {
+      position: "top-center",
+      autoClose: 4000,
+    });
+  };
 
   const handleClick = (donation) => {
-    setConfirmation(true);
-    setSelectedDonation(donation);
-    db.collection("Donations").doc(donation.id).set(
-      {
-        Status: false,
-        recipientId: currentUser.uid,
-      },
-      { merge: true }
-    );
-    history.push({
-      pathname: "/confirmation",
-      state: {
-        donation,
-        supplierInfo,
-      },
-    });
+    if (currentUser) {
+      setConfirmation(true);
+      setSelectedDonation(donation);
+      db.collection("Donations").doc(donation.id).set(
+        {
+          Status: false,
+          recipientId: currentUser.uid,
+        },
+        { merge: true }
+      );
+      history.push({
+        pathname: "/confirmation",
+        state: {
+          donation,
+          supplierInfo,
+        },
+      });
+    } else {
+      showToast();
+    }
   };
 
   return (

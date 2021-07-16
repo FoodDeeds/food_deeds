8c8d629f671dda6e81e38d9051f6641b7b2fa812
Pauline Tannos <ptannos2@gmail.com>
2021-07-16 15:56:01 -0400

    {feat}: graph renders with real data

--- src/components/GivingHistory.js
+++ src/components/GivingHistory.js
@@ -7,142 +7,135 @@ import { useHistory } from "react-router-dom";
 import Graphs from "./Graphs";
 
 const GivingHistory = (props) => {
-    const userInfo = props.userInfo;
-    const [donations, setDonations] = useState([]);
+  const userInfo = props.userInfo;
+  const [donations, setDonations] = useState([]);
 
-    const history = useHistory();
+  const history = useHistory();
 
-    useEffect(() => {
-        db.collection("Donations")
-            .where("supplierId", "==", `${userInfo.id}`)
-            .onSnapshot((snapshot) => {
-                setDonations(
-                    snapshot.docs.map((doc) => ({
-                        id: doc.id,
-                        info: doc.data()
-                    }))
-                );
-            });
-    }, [userInfo.id]);
-
-    const handleCancel = (donation) => {
-        toast.configure();
-        const notify = () => {
-            toast("This donation has been cancelled.");
-        };
-        notify();
-        db.collection("Donations").doc(donation.id).set(
-            {
-                Status: null
-            },
-            { merge: true }
+  useEffect(() => {
+    db.collection("Donations")
+      .where("supplierId", "==", `${userInfo.id}`)
+      .onSnapshot((snapshot) => {
+        setDonations(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            info: doc.data(),
+          }))
         );
+      });
+  }, [userInfo.id]);
+
+  const handleCancel = (donation) => {
+    toast.configure();
+    const notify = () => {
+      toast("This donation has been cancelled.");
     };
-
-    const handleEdit = (donation) => {
-        history.push({
-            pathname: "/donationedit",
-            state: {
-                donation,
-                userInfo
-            }
-        });
-    };
-
-    const totalQty = function () {
-        let total = 0;
-        for (let i = 0; i < donations.length; i++) {
-            if (donations[i].info.Status !== null) {
-                let qty = donations[i].info.Quantity;
-                total += Number.parseInt(qty);
-            }
-        }
-        return total;
-    };
-
-    return (
-        <div>
-            <Graphs donations={donations} />
-
-            <Header style={{ marginTop: 20, marginLeft: 30 }}>
-                Giving History
-            </Header>
-            <Item.Description style={{ marginLeft: 30 }}>
-                You have donated a total of {totalQty()} boxes of food. Way to
-                go!
-            </Item.Description>
-            {donations.map((donation) => (
-                <Segment
-                    className="result"
-                    key={donation.id}
-                    style={{ width: 300, marginLeft: 30 }}
-                >
-                    {/* <Item.Group divided style={{ marginLeft: 30 }}> */}
-                    <Item>
-                        <br />
-                        <Item.Content
-                            style={{ marginTop: -20, marginBottom: 20 }}
-                        >
-                            <Item.Description>
-                                {donation.info.Description}
-                            </Item.Description>
-                            <Item.Meta>
-                                {donation.info.PickupDate}
-                                <br />
-                                at {donation.info.PickupTime} <br />
-                            </Item.Meta>
-                            Quantity: {donation.info.Quantity} boxes
-                            <br />
-                        </Item.Content>
-
-                        {donation.info.Status === null ? (
-                            <p>
-                                <Button
-                                    basic
-                                    color="red"
-                                    style={{
-                                        width: 100,
-                                        height: 35,
-                                        marginRight: 20
-                                    }}
-                                >
-                                    Canceled
-                                </Button>
-                            </p>
-                        ) : (
-                            <p>
-                                <Button
-                                    basic
-                                    color="green"
-                                    onClick={() => handleEdit(donation)}
-                                    style={{
-                                        width: 100,
-                                        height: 35,
-                                        marginRight: 20
-                                    }}
-                                >
-                                    Edit
-                                </Button>
-                                <Button
-                                    basic
-                                    onClick={() => handleCancel(donation)}
-                                    color="green"
-                                    style={{
-                                        width: 100,
-                                        height: 35,
-                                        marginRight: 20
-                                    }}
-                                >
-                                    Cancel
-                                </Button>
-                            </p>
-                        )}
-                    </Item>
-                    {/* </Item.Group> */}
-                </Segment>
-            ))}
-        </div>
+    notify();
+    db.collection("Donations").doc(donation.id).set(
+      {
+        Status: null,
+      },
+      { merge: true }
     );
+  };
+
+  const handleEdit = (donation) => {
+    history.push({
+      pathname: "/donationedit",
+      state: {
+        donation,
+        userInfo,
+      },
+    });
+  };
+
+  const totalQty = function () {
+    let total = 0;
+    for (let i = 0; i < donations.length; i++) {
+      if (donations[i].info.Status !== null) {
+        let qty = donations[i].info.Quantity;
+        total += Number.parseInt(qty);
+      }
+    }
+    return total;
+  };
+
+  return (
+    <div>
+      <Graphs donations={donations} />
+
+      <Header style={{ marginTop: 20, marginLeft: 30 }}>Giving History</Header>
+      <Item.Description style={{ marginLeft: 30 }}>
+        You have donated a total of {totalQty()} boxes of food. Way to go!
+      </Item.Description>
+      {donations.map((donation) => (
+        <Segment
+          className="result"
+          key={donation.id}
+          style={{ width: 300, marginLeft: 30 }}
+        >
+          {/* <Item.Group divided style={{ marginLeft: 30 }}> */}
+          <Item>
+            <br />
+            <Item.Content style={{ marginTop: -20, marginBottom: 20 }}>
+              <Item.Description>{donation.info.Description}</Item.Description>
+              <Item.Meta>
+                {donation.info.PickupDate}
+                <br />
+                at {donation.info.PickupTime} <br />
+              </Item.Meta>
+              Quantity: {donation.info.Quantity} boxes
+              <br />
+            </Item.Content>
+
+            {donation.info.Status === null ? (
+              <p>
+                <Button
+                  basic
+                  color="red"
+                  style={{
+                    width: 100,
+                    height: 35,
+                    marginRight: 20,
+                  }}
+                >
+                  Canceled
+                </Button>
+              </p>
+            ) : (
+              <p>
+                <Button
+                  basic
+                  color="green"
+                  onClick={() => handleEdit(donation)}
+                  style={{
+                    width: 100,
+                    height: 35,
+                    marginRight: 20,
+                  }}
+                >
+                  Edit
+                </Button>
+                <Button
+                  basic
+                  onClick={() => handleCancel(donation)}
+                  color="green"
+                  style={{
+                    width: 100,
+                    height: 35,
+                    marginRight: 20,
+                  }}
+                >
+                  Cancel
+                </Button>
+              </p>
+            )}
+          </Item>
+          {/* </Item.Group> */}
+        </Segment>
+      ))}
+    </div>
+  );
 };
 
 export default GivingHistory;
--- src/components/Graphs.js
+++ src/components/Graphs.js
@@ -1,77 +1,80 @@
 import React from "react";
-import { Bar, Pie, Line, Radar } from "react-chartjs-2";
+import { Pie } from "react-chartjs-2";
 
 const Graphs = ({ donations }) => {
-    const filterDate = (month) => {
-        const julyDonation = donations.filter((july) =>
-            july.info.PickupDate.includes(month)
-        );
-        let total = 0;
-        for (let i = 0; i < julyDonation.length; i++) {
-            if (julyDonation[i].info.Status !== null) {
-                let qty = julyDonation[i].info.Quantity;
-                total += Number.parseInt(qty);
-            }
-        }
-        return total;
-    };
-
-    console.log("july>>>", filterDate());
-
-    const labels = ["2021-05", "2021-06", "2021-07"];
-    // data
-
-    return (
-        <div>
-            <Bar
-                data={{
-                    labels: labels,
-                    datasets: [
-                        {
-                            label: "# of votes",
-                            data: [12, 19, filterDate(labels[2])],
-                            backgroundColor: [
-                                "rgba(255, 99, 132, 0.2)",
-                                "rgba(54, 162, 235, 0.2)",
-                                "rgba(255, 206, 86, 0.2)"
-                            ],
-                            borderColor: [
-                                "rgba(255, 99, 132, 1)",
-                                "rgba(54, 162, 235, 1)",
-                                "rgba(255, 206, 86, 1)"
-                            ],
-                            borderWidth: 1
-                        }
-                        // {
-                        //   label: 'Quantity',
-                        //   data: [47, 52, 67, 58, 9, 50],
-                        //   backgroundColor: 'orange',
-                        //   borderColor: 'red',
-                        // },
-                    ]
-                }}
-                height={400}
-                width={100}
-                options={{
-                    maintainAspectRatio: false,
-                    scales: {
-                        yAxes: [
-                            {
-                                ticks: {
-                                    beginAtZero: true
-                                }
-                            }
-                        ]
-                    },
-                    legend: {
-                        labels: {
-                            fontSize: 25
-                        }
-                    }
-                }}
-            />
-        </div>
+  const filterDate = (month) => {
+    const julyDonation = donations.filter((july) =>
+      july.info.PickupDate.includes(month)
     );
+    let total = 0;
+    for (let i = 0; i < julyDonation.length; i++) {
+      if (julyDonation[i].info.Status !== null) {
+        let qty = julyDonation[i].info.Quantity;
+        total += Number.parseInt(qty);
+      }
+    }
+    return total;
+  };
+
+  console.log("july>>>", filterDate());
+
+  const labels = () => {
+    let result = [];
+
+    const dates = donations.map((donation) => {
+      const datesSliced = donation.info.PickupDate.slice(0, 7);
+      return datesSliced;
+    });
+
+    for (let i = 0; i < dates.length; i++) {
+      if (!result.includes(dates[i])) {
+        result.push(dates[i]);
+      }
+    }
+    let newLabel = result.slice(-3);
+    return newLabel;
+  };
+
+  const data = labels().map((label) => filterDate(label));
+  console.log("labels", labels());
+  console.log("data", data);
+  return (
+    <div>
+      <Pie
+        data={{
+          labels: labels(),
+          datasets: [
+            {
+              label: "# of votes",
+              data: data,
+              backgroundColor: [
+                "rgba(255, 99, 132, 0.2)",
+                "rgba(54, 162, 235, 0.2)",
+                "rgba(255, 206, 86, 0.2)",
+              ],
+              borderColor: [
+                "rgba(255, 99, 132, 1)",
+                "rgba(54, 162, 235, 1)",
+                "rgba(255, 206, 86, 1)",
+              ],
+              borderWidth: 1,
+            },
+          ],
+        }}
+        height={400}
+        width={100}
+        options={{
+          maintainAspectRatio: false,
+
+          legend: {
+            labels: {
+              fontSize: 25,
+            },
+          },
+        }}
+      />
+    </div>
+  );
 };
 
 export default Graphs;

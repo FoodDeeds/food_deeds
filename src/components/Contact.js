658783be64cfa76d053e3f818946712c61c4781d
Laura Krey <80431530+lkrey@users.noreply.github.com>
2021-07-16 15:26:23 -0400

    {update}: contact us layout, {feat}: contact from in prog

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,14 @@
-import React from "react";
-import { List, Header } from "semantic-ui-react";
+import React, { useState, useEffect } from "react";
+import { Header, Form, Button, Image, List } from "semantic-ui-react";
 import { useHistory } from "react-router-dom";
+import { db } from "../firebase";
 
 const Contact = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [loader, setLoader] = useState(false);
+
   const history = useHistory();
 
   const handleClick = () => {
@@ -11,40 +17,107 @@ const Contact = () => {
     });
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoader(true);
+
+    db.collection("SignedUpUsers")
+      .add({
+        name: name,
+        email: email,
+        message: message,
+      })
+      .then(() => {
+        setLoader(false);
+        alert("Your message has been submitted👍");
+      })
+      .catch((error) => {
+        alert(error.message);
+        setLoader(false);
+      });
+
+    setName("");
+    setEmail("");
+    setMessage("");
+  };
+
   return (
-    <List>
-      <Header size="medium" style={{ marginTop: 30 }}>
-        Ready to help us fight food insecurity?
-      </Header>
-      <Header as="h4">
-        Get in touch with Food Deeds today.</Header>
-      <List.Item
-        icon="phone"
-        content="1-800-GIV-FOOD"
+    <div>
+      <Form>
+        <Header
+          as="h2"
+          color="green"
+          style={{ marginLeft: 35 }}
+          textAlign="center"
+        >
+          Contact Us
+        </Header>
+        <Form.Field>
+          <label style={{ marginLeft: 25 }}>Name</label>
+          <input
+            required
+            placeholder="Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            style={{ marginLeft: 20, width: 350, marginRight: 20 }}
+          />
+          <br />
+        </Form.Field>
+        <Form.Field>
+          <label style={{ marginLeft: 25 }}>Email</label>
+          <input
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            style={{ marginLeft: 20, width: 350, marginRight: 30 }}
+          />
+        </Form.Field>
+        <Form.Field>
+          <label style={{ marginLeft: 25 }}>Message</label>
+          <textarea
+            placeholder="Message"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            style={{ marginLeft: 20, width: 350, marginRight: 30 }}
+          ></textarea>
+        </Form.Field>
+      </Form>
+      <br />
+      <Button
+        type="submit"
         color="green"
-        style={{ marginLeft: 30 }}
-      />
-      <List.Item
-        icon="marker"
-        content="New York, NY"
-        color="green"
-        style={{ marginLeft: 30 }}
-      />
-      <List.Item
-        icon="mail"
-        content="support@fooddeeds.com"
-        color="green"
-        style={{ marginLeft: 30 }}
-      />
-      <List.Item
-        as="a"
-        icon="linkify"
-        content="Food Deeds FAQ"
-        color="green"
-        onClick={handleClick}
-        style={{ marginLeft: 30 }}
-      />
-    </List>
+        style={{ marginLeft: 125, background: loader ? "red" : "green" }}
+      >
+        Submit Inquiry
+      </Button>
+      <List>
+        <Header textAlign="center" size="medium" style={{ marginTop: 30 }}>
+          Ready to help us fight food insecurity?
+       <br/>
+       Get in touch with Food Deeds today.
+        </Header>
+        <List.Item
+          icon="phone"
+          content="1-800-GIV-FOOD"
+          color="green"
+          style={{ marginLeft: 30 }}
+        />
+        <List.Item
+          icon="marker"
+          content="New York, NY 10012"
+          color="green"
+          style={{ marginLeft: 30 }}
+        />
+        <List.Item
+          as="a"
+          icon="linkify"
+          content="Food Deeds FAQ"
+          color="green"
+          onClick={handleClick}
+          style={{ marginLeft: 30 }}
+        />
+      </List>
+    </div>
   );
 };
 
